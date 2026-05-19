7cfff5ec5efac5b569ce93ad9d74e99672803bb6
push-shihab <shihabfamily539@gmail.com>
2026-05-19 18:09:45 +0600

    Database connected successfully

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+app.use(express.json());
 dotenv.config();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("server is flying");
 });
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.a82ocix.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("DB connected successfully");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is flying on port ${port}`);
